bd4bc1f19c2012bc96153433aa051c73edcd5c30
elipri <elina.prints@gmail.com>
2021-01-11 23:52:28 +0200

    Ajutine lahendus taustale

--- avaleht.php
+++ avaleht.php
@@ -34,9 +34,9 @@
     </div>
     <div id="scene">
       <div data-depth="0.3" > 
-        <img src="img/Taust.png" width="100%" height="auto">
+        <img src="img/Taust.png">
       </div>
-      <div data-depth="0.3">
+      <!-- <div data-depth="0.3">
         <span class="dot dot1"></span>
       </div>
       <div data-depth="0.3">
@@ -47,7 +47,7 @@
       </div>
       <div data-depth="0.3">
         <span class="dot dot4"></span>
-      </div>
+      </div> -->
     </div>
     <div class="wrap">
         <nav>
